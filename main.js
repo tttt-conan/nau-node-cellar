1ff19f3fbf89df0d11757d1babdac5297b18a0d0
Trần Trọng Thanh Tùng <tttt.conan@gmail.com>
2014-08-01 21:32:01 +0700

    create view detail div and adding some inputs

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-	$.ajax({
-		dataType: "json",
-		url: "http://dev.naustud.io:3000/wines",
-		success: function(wineList){
-			function renderList(wine){
-				var temp = "generic.jpg";
-				if(wine.hasOwnProperty("picture") === true)
-					temp = wine.picture;
-				return '<li><a href="/'+ wine._id +'"><img src="http://dev.naustud.io:3000/pics/'+ temp
-				+'" alt="'+wine.name+'"><h5>'+wine.name+'</h5>'+wine.year+' '+wine.grapes
-				+'<br>'+wine.region+', '+wine.country+'</a></li>';
+	if($("#wineList").hasClass("active")){
+		$.ajax({
+			dataType: "json",
+			url: "http://dev.naustud.io:3000/wines",
+			success: function(wineList){
+				function renderList(wine){
+					var temp = "generic.jpg";
+					if(wine.hasOwnProperty("picture") === true)
+						temp = wine.picture;
+					return '<li><a href="/'+ wine._id +'"><img src="http://dev.naustud.io:3000/pics/'+ temp
+					+'" alt="'+wine.name+'"><h5>'+wine.name+'</h5>'+wine.year+' '+wine.grapes
+					+'<br>'+wine.region+', '+wine.country+'</a></li>';
+				}
+				for(var i = 0; i < wineList.length; i++){
+					var layout = renderList(wineList[i]);
+					$("#wineList ul").append(layout);
+				}
 			}
-			for(var i = 0; i < wineList.length; i++){
-				var layout = renderList(wineList[i]);
-				$("#wineList ul").append(layout);
-			}
-		}
-	});
+		});
+	}
 });

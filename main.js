777e7dfffe46e3f49c6f228a1edb38c6fe76e8b2
Trần Trọng Thanh Tùng <tttt.conan@gmail.com>
2014-08-04 10:58:38 +0700

    Adding browse wine navigation

--- main.js
+++ main.js
@@ -18,10 +18,35 @@ $(document).ready(function() {
 		}
 	});
 
+
+	$("#browseWine").on("click",function() {
+		$("#viewDetails").addClass("hidden");
+		$("#viewDetails").removeClass("active");
+		$.ajax({
+			dataType: "json",
+			url: "http://dev.naustud.io:3000/wines",
+			success: function(wineList){
+				function renderList(wine){
+					var temp = "generic.jpg";
+					if(wine.hasOwnProperty("picture") === true)
+						temp = wine.picture;
+					return '<li><a data-id="'+ wine._id +'"><img src="http://dev.naustud.io:3000/pics/'+ temp
+					+'" alt="'+wine.name+'"><h5>'+wine.name+'</h5>'+wine.year+' '+wine.grapes
+					+'<br>'+wine.region+', '+wine.country+'</a></li>';
+				}
+				for(var i = 0; i < wineList.length; i++){
+					var layout = renderList(wineList[i]);
+					$("#wineList ul").append(layout);
+				}
+			}
+		});
+	});
+
+
 	$("#wineList ul").on("click","a",function() {
 		$("#wineList").find("ul").empty();
 		$("#viewDetails").removeClass("hidden");
-		$("#viewDetails").removeClass("active");
+		$("#viewDetails").addClass("active");
 		var serviceURL = "http://dev.naustud.io:3000/wines/" + $(this).data("id");
 		$.ajax({
 			dataType: "json",

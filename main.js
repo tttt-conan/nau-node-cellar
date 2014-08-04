ab82b1e339cdd8ee2aece79e0e4c563ef68f3590
Trần Trọng Thanh Tùng <tttt.conan@gmail.com>
2014-08-04 10:45:46 +0700

    loading wine info with ajax

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 $(document).ready(function() {
-	if($("#wineList").hasClass("active")){
+	$.ajax({
+		dataType: "json",
+		url: "http://dev.naustud.io:3000/wines",
+		success: function(wineList){
+			function renderList(wine){
+				var temp = "generic.jpg";
+				if(wine.hasOwnProperty("picture") === true)
+					temp = wine.picture;
+				return '<li><a data-id="'+ wine._id +'"><img src="http://dev.naustud.io:3000/pics/'+ temp
+				+'" alt="'+wine.name+'"><h5>'+wine.name+'</h5>'+wine.year+' '+wine.grapes
+				+'<br>'+wine.region+', '+wine.country+'</a></li>';
+			}
+			for(var i = 0; i < wineList.length; i++){
+				var layout = renderList(wineList[i]);
+				$("#wineList ul").append(layout);
+			}
+		}
+	});
+
+	$("#wineList ul").on("click","a",function() {
+		$("#wineList").find("ul").empty();
+		$("#viewDetails").removeClass("hidden");
+		$("#viewDetails").removeClass("active");
+		var serviceURL = "http://dev.naustud.io:3000/wines/" + $(this).data("id");
 		$.ajax({
 			dataType: "json",
-			url: "http://dev.naustud.io:3000/wines",
-			success: function(wineList){
-				function renderList(wine){
-					var temp = "generic.jpg";
-					if(wine.hasOwnProperty("picture") === true)
-						temp = wine.picture;
-					return '<li><a href="/'+ wine._id +'"><img src="http://dev.naustud.io:3000/pics/'+ temp
-					+'" alt="'+wine.name+'"><h5>'+wine.name+'</h5>'+wine.year+' '+wine.grapes
-					+'<br>'+wine.region+', '+wine.country+'</a></li>';
-				}
-				for(var i = 0; i < wineList.length; i++){
-					var layout = renderList(wineList[i]);
-					$("#wineList ul").append(layout);
-				}
+			url: serviceURL,
+			success: function(wine){
+				$("#_id").val(wine._id);
+				$("#name").val(wine.name);
+				$("#grapes").val(wine.grapes);
+				$("#country").val(wine.country);
+				$("#region").val(wine.region);
+				$("#year").val(wine.year);
+				$("#description").text(wine.description);
+				$("#picture").attr("src","http://dev.naustud.io:3000/pics/"+(wine.picture||"generic.jpg"));
 			}
 		});
-	}
+	});
+
 });

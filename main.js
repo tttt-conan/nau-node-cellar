b9f356659c900e92475408872aea4a63cf45df11
Trần Trọng Thanh Tùng <tttt.conan@gmail.com>
2014-08-06 12:04:04 +0700

    Adding add-wine, delete-wine and update-wine functions

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+function loadBrowsePage(){
+	$("#viewDetails").addClass("hidden");
+	$("#viewDetails").removeClass("active");
 	$.ajax({
 		dataType: "json",
 		url: "http://dev.naustud.io:3000/wines",
@@ -17,29 +19,30 @@ $(document).ready(function() {
 			}
 		}
 	});
+}
 
+$(document).ready(function() {
+	loadBrowsePage();
 
 	$("#browseWine").on("click",function() {
-		$("#viewDetails").addClass("hidden");
-		$("#viewDetails").removeClass("active");
-		$.ajax({
-			dataType: "json",
-			url: "http://dev.naustud.io:3000/wines",
-			success: function(wineList){
-				function renderList(wine){
-					var temp = "generic.jpg";
-					if(wine.hasOwnProperty("picture") === true && wine.picture !== null)
-						temp = wine.picture;
-					return '<li><a data-id="'+ wine._id +'"><img src="http://dev.naustud.io:3000/pics/'+ temp
-					+'" alt="'+wine.name+'"><h5>'+wine.name+'</h5>'+wine.year+' '+wine.grapes
-					+'<br>'+wine.region+', '+wine.country+'</a></li>';
-				}
-				for(var i = 0; i < wineList.length; i++){
-					var layout = renderList(wineList[i]);
-					$("#wineList ul").append(layout);
-				}
-			}
-		});
+		if($("#viewDetails").hasClass("hidden") === true)
+			return;
+
+		loadBrowsePage();
+	});
+
+	$("#addWine").on("click",function() {
+		$("#_id").val("");
+		$("#name").val("");
+		$("#grapes").val("");
+		$("#country").val("");
+		$("#region").val("");
+		$("#year").val("");
+		$("#description").text.empty();
+		$("#picture").attr("src","http://dev.naustud.io:3000/pics/generic.jpg");
+		$("#wineList").find("ul").empty();
+		$("#viewDetails").removeClass("hidden");
+		$("#viewDetails").addClass("active");
 	});
 
 
@@ -63,5 +66,52 @@ $(document).ready(function() {
 			}
 		});
 	});
+
+	$(".deleteBttn").on("click",function() {
+		var serviceURL = "http://dev.naustud.io:3000/wines/" + $("#_id").val();
+		$.ajax({
+			type: "delete",
+			url: serviceURL,
+			success: function(){
+				alert("wine deleted successfully");
+				loadBrowsePage();
+			}
+		})
+	});
+
+	$(".saveBttn").on("click",function() {
+		var serviceURL = "http://dev.naustud.io:3000/wines";
+		var serviceType = "post";
+		if($("#_id").val() !== ""){
+			serviceURL += "/";
+			serviceURL += $("#_id").val();
+			serviceType = "put";
+		}
+		var dataOb = {
+			name: $("#name").val(),
+			grapes:	$("#grapes").val(),
+			country: $("#country").val(),
+			region: $("#region").val(),
+			year: $("#year").val(),
+			description: $("#description").text(),
+		};
+		$.ajax({
+			type: serviceType,
+			data: dataOb,
+			dataType: 'json',
+			url: serviceURL,
+			success: function(response){
+				console.log(response);
+				$("#viewDetails").append("<div class='successText'><strong>Success!</strong> wine updated successfully</div>");
+				$(".successText").css({
+					'color':'#468847',
+					'background-color':'#dff0d8',
+				});
+				$(".successText").fadeOut(5000);
+			}
+		})
+		event.preventDefault();
+	});
+
 
 });
